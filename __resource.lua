a6b6e42620a2203e27bc25969c4adac0ec5fd1fd
Guudz <Kilian1702@outlook.com>
2023-02-03 03:34:35 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,51 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'FiveM New HUD for ESX'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
+
+files {
+	'html/ui.html',
+	'html/style.css',
+	'html/grid.css',
+	'html/main.js',
+	'html/img/wallet.png',
+	'html/img/credit-card.png',
+	'html/img/jobs/banker.png',
+	'html/img/jobs/unicorn.png',
+	'html/img/jobs/bus.png',
+	'html/img/jobs/cardealer.png',
+	'html/img/jobs/detective.png',
+	'html/img/jobs/ambulance.png',
+	'html/img/jobs/tailor.png',
+	'html/img/jobs/fisherman.png',
+	'html/img/jobs/garbage.png',
+	'html/img/jobs/lumberjack.png',
+	'html/img/jobs/slaughterer.png',
+	'html/img/jobs/mechanic.png',
+	'html/img/jobs/miner.png',
+	'html/img/jobs/pizza.png',
+	'html/img/jobs/police.png',
+	'html/img/jobs/realestateagent.png',
+	'html/img/jobs/banksecurity.png',
+	'html/img/jobs/sheriff.png',
+	'html/img/jobs/swat.png',
+	'html/img/jobs/fueler.png',
+	'html/img/jobs/trucker.png',
+	'html/img/jobs/taxi.png',
+	'html/img/jobs/unemployed.png',
+	'html/img/hunger.png',
+	'html/img/water.png',
+	'html/img/speaker1.png',
+	'html/img/speaker2.png',
+	'html/img/speaker3.png',
+	'html/fonts/ANDRY.ttf',
+}
---
+++ html/main.js
@@ -0,0 +1,15 @@
+var rgbStart = [139,195,74]
+var rgbEnd = [183,28,28]
+//API Shit
+function colourGradient(p, rgb_beginning, rgb_end)
+{
+    var w = p * 2 - 1;
+
+    var w1 = (w + 1) / 2.0;
+    var w2 = 1 - w1;
+
+    var rgb = [parseInt(rgb_beginning[0] * w1 + rgb_end[0] * w2),
+        parseInt(rgb_beginning[1] * w1 + rgb_end[1] * w2),
+            parseInt(rgb_beginning[2] * w1 + rgb_end[2] * w2)];
+    return rgb;
+};
